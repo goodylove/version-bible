597aaff64b93db8f1b7320ad16bdc2d0ff5d7314
goodness <goodyc474@gmail.com>
2023-06-09 16:58:36 +0100

    style navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
 import MobileMenu from "./Components/MobileMenu/index"
 import "./App.css"
 import Footer from "./Components/Footer"
+import NavBar from "./Components/NavBar"
 
 function App() {
   return (
-    <div className="App">
-      <MobileMenu />
-      <Footer />
+    <div className="App ">
+      <NavBar />
     </div>
   )
 }
