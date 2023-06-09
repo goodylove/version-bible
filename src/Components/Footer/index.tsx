f3285d36de05970045be5cad4141cbad160df7af
goodness <goodyc474@gmail.com>
2023-06-09 06:22:57 +0100

    install-react-icons-package

---
+++ src/Components/Footer/index.tsx
@@ -0,0 +1,9 @@
+import React from "react"
+import { GoHome } from "react-icons/go"
+import { BiBible } from "react-icons/bi"
+
+const Footer = () => {
+  return <div>Footer</div>
+}
+
+export default Footer
