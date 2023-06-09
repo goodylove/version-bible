b8d4d63f5c09dbe4838b205a9eb976ba5f21ef83
goodness <goodyc474@gmail.com>
2023-06-09 09:41:22 +0100

    completed mobile menu section

--- src/Components/MobileMenu/index.tsx
+++ src/Components/MobileMenu/index.tsx
@@ -3,7 +3,7 @@ import { Svg } from "../Svg/svg"
 
 const MobileMenu = () => {
   return (
-    <section className="w-full h-4 flex justify-center  items-center  px-5  py-10 bg-slate-200 shadow-md">
+    <section className="w-full h-4 flex justify-center  items-center  px-5  py-10 bg-slate-200 shadow-md lg:hidden">
       <div className="flex justify-between w-full text-[16px]">
         <span className="flex flex-col justify-center items-center">
           {Svg.home()}Home
