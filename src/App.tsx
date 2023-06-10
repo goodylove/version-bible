c9478725db4ad573e3958c2f6d73537c28e3d6cf
goodness <goodyc474@gmail.com>
2023-06-10 14:48:50 +0100

    completed navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,17 @@
 import MobileMenu from "./Components/MobileMenu/index"
-import "./App.css"
 import Footer from "./Components/Footer"
 import NavBar from "./Components/NavBar"
+import { Routes, Route } from "react-router-dom"
+import Home from "./pages/Home"
+import "./App.css"
 
 function App() {
   return (
     <div className="App ">
       <NavBar />
-      <MobileMenu />
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
     </div>
   )
 }
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,7 +1,29 @@
 import React from "react"
 
 function Home() {
-  return <div>Home</div>
+  return (
+    <main className="w-full">
+      <section>
+        <h2>Get a free bible for your phone and tablet</h2>
+        <p>online and offline bible is avaliable any time.</p>
+        <p>No ads No Purchase</p>
+
+        <div>
+          <img
+            src="https://www.bible.com/_next/image?url=%2Fassets%2Fimages%2Fqr-home.png&w=256&q=75"
+            alt=""
+          />
+          <div>
+            <img
+              width={50}
+              src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTdo4V3uVC96vQ2wpvkXUY3Hkx8DbaNcWW1fXgIL2uFIb80XUez"
+              alt=""
+            />
+          </div>
+        </div>
+      </section>
+    </main>
+  )
 }
 
 export default Home
