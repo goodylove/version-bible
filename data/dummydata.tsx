10e64d7b181b9a17d3800ae74a38891e5d5d477e
goodness <goodyc474@gmail.com>
2023-06-11 17:42:59 +0100

    created data folder

---
+++ data/dummydata.tsx
@@ -0,0 +1,56 @@
+export const PlansData = [
+  {
+    img: "https://www.bible.com/_next/image?url=https%3A%2F%2Fimageproxy.youversionapi.com%2Fhttps%3A%2F%2Fs3.amazonaws.com%2Fyvplans%2F34110%2F720x405.jpg&w=1920&q=75",
+    title: "Kids Bible Experience | Who Am I?",
+    day: "4 Days ",
+    start: "start plan",
+  },
+  {
+    img: "https://www.bible.com/_next/image?url=https%3A%2F%2Fimageproxy.youversionapi.com%2Fhttps%3A%2F%2Fs3.amazonaws.com%2Fyvplans%2F31087%2F720x405.jpg&w=1920&q=75",
+    title: "Fiercely Loved",
+    day: " 7 Days ",
+    start: "start plan",
+  },
+  {
+    img: "https://www.bible.com/_next/image?url=https%3A%2F%2Fimageproxy.youversionapi.com%2Fhttps%3A%2F%2Fs3.amazonaws.com%2Fyvplans%2F36395%2F720x405.jpg&w=1920&q=75",
+    title: "Renamed",
+    day: " 7 Days ",
+    start: "start plan",
+  },
+  {
+    img: "https://www.bible.com/_next/image?url=https%3A%2F%2Fimageproxy.youversionapi.com%2Fhttps%3A%2F%2Fs3.amazonaws.com%2Fyvplans%2F36907%2F720x405.jpg&w=1920&q=75",
+    title: "Who Am I Really? A Journey to Discover Your True Identity",
+    day: " 7 Days ",
+    start: "start plan",
+  },
+  {
+    img: "https://www.bible.com/_next/image?url=https%3A%2F%2Fimageproxy.youversionapi.com%2Fhttps%3A%2F%2Fs3.amazonaws.com%2Fyvplans%2F32378%2F720x405.jpg&w=1920&q=75",
+    title: "Know the Power of I Am",
+    day: " 7 Days ",
+    start: "start plan",
+  },
+  {
+    img: "https://www.bible.com/_next/image?url=https%3A%2F%2Fimageproxy.youversionapi.com%2Fhttps%3A%2F%2Fs3.amazonaws.com%2Fyvplans%2F36687%2F720x405.jpg&w=1920&q=75",
+    title: "Who Am I",
+    day: " 20  Days ",
+    start: "start plan",
+  },
+  {
+    img: "https://www.bible.com/_next/image?url=https%3A%2F%2Fimageproxy.youversionapi.com%2Fhttps%3A%2F%2Fs3.amazonaws.com%2Fyvplans%2F36160%2F720x405.jpg&w=1920&q=75",
+    title: "7 Days of Building Courage",
+    day: " 7 Days ",
+    start: "start plan",
+  },
+  {
+    img: "https://www.bible.com/_next/image?url=https%3A%2F%2Fimageproxy.youversionapi.com%2Fhttps%3A%2F%2Fs3.amazonaws.com%2Fyvplans%2F36826%2F720x405.jpg&w=1920&q=75",
+    title: "Don't Look Back",
+    day: " 7 Days ",
+    start: "start plan",
+  },
+  {
+    img: "https://www.bible.com/_next/image?url=https%3A%2F%2Fimageproxy.youversionapi.com%2Fhttps%3A%2F%2Fs3.amazonaws.com%2Fyvplans%2F31669%2F720x405.jpg&w=1920&q=755",
+    title: "Her True Worth: A 5-Day Plan",
+    day: " 5 Days ",
+    start: "start plan",
+  },
+]
