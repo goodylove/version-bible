570cb816666a81a0c99f3cf4a626ca38a46ce113
goodness <goodyc474@gmail.com>
2023-06-16 16:16:07 +0100

    added slider

--- src/Components/Helper/ExtraData.ts
+++ src/Components/Helper/ExtraData.ts
@@ -5,3 +5,32 @@ export const randomImage: randomImageProps = [
   "https://www.bible.com/_next/image?url=https%3A%2F%2Fimageproxy.youversionapi.com%2F640x640%2Fhttps%3A%2F%2Fs3.amazonaws.com%2Fstatic-youversionapi-com%2Fimages%2Fbase%2F82511%2F1280x1280.jpg&w=1920&q=75",
   "https://www.bible.com/_next/image?url=https%3A%2F%2Fimageproxy.youversionapi.com%2F640x640%2Fhttps%3A%2F%2Fs3.amazonaws.com%2Fstatic-youversionapi-com%2Fimages%2Fbase%2F58093%2F1280x1280.jpg&w=640&q=75",
 ]
+
+export const categories = [
+  {
+    name: " Love",
+    backgroundImage: `linear-gradient(45deg, rgb(110, 52, 125) 0%, rgb(196, 75, 149) 43%, rgb(218, 98, 98) 86%)`,
+  },
+  {
+    name: "Anxiety",
+    backgroundImage: `linear-gradient(45deg, rgb(33, 87, 54) 0%, rgb(68, 179, 110) 75%, rgb(68, 179, 110) 100%)`,
+  },
+  {
+    name: "Kids",
+    backgroundImage: ` linear-gradient(45deg, rgb(108, 35, 35) 0%, rgb(196, 177, 75) 86%, rgb(196, 177, 75) 100%)`,
+  },
+  {
+    name: "Hope",
+    backgroundImage:
+      "linear-gradient(45deg, rgb(52, 119, 123) 0%, rgb(255, 236, 153) 100%)",
+  },
+
+  {
+    name: "Fear",
+    backgroundImage: `linear-gradient(45deg, rgb(76, 44, 115) 0%, rgb(200, 109, 215) 95%)`,
+  },
+  {
+    name: "Depressions",
+    backgroundImage: `linear-gradient(45deg, rgb(28, 62, 74) 0%, rgb(51, 114, 135) 54%, rgb(86, 192, 227) 97%)`,
+  },
+]
--- src/pages/Plans/index.tsx
+++ src/pages/Plans/index.tsx
@@ -3,44 +3,84 @@ import { GrPrevious, GrNext } from "react-icons/gr"
 import { useAppSelector, useAppDispatch } from "../../app/hooks"
 import { planCardProps, PlanCardTwoProps } from "../../types"
 import PlanCardTwo from "../../Components/PlanCardTwo"
+import { categories } from "../../Components/Helper/ExtraData"
 
 function Plans() {
   const dispatch = useAppDispatch()
   const data = useAppSelector((state) => state.readplan)
-  const dataleng = useAppSelector((state) => state.readplan.plans)
+
   const [currentState, setCurrentState] = React.useState<number>(0)
-  let isClicked = false
-  const ref = React.useRef<HTMLImageElement>(null)
+  const [currenntSlid, setCurrenntSlid] = React.useState<number>(0)
 
-  const HandleRightClik = () => {
-    setCurrentState((prevIndex) =>
-      prevIndex === data.plans.random?.length - 3 ? 0 : prevIndex + 1,
-    )
-
-    isClicked = true
+  const HandleRight = (param: any, param2: any) => {
+    param((prevIndex: number) => (prevIndex === param2 ? 0 : prevIndex + 1))
   }
-  const handleLeft = () => {
-    setCurrentState((prevIndex) =>
-      prevIndex === 0 ? data.plans.random?.length - 3 : prevIndex - 1,
-    )
+
+  const handleLeft = (param: any, param2: any) => {
+    param((prevIndex: number) => (prevIndex === 0 ? param2 : prevIndex - 1))
+  }
+
+  const styleCarousel = {
+    transform: `translateX(-${currentState}00%)`,
   }
 
   return (
-    <main className="w-full">
+    <main className="w-[100vw]">
       <section className="w-[100vw] relative">
-        <div>
-          <span className=" fixed  top-[18rem] z-50  text-[20px] pl-4">
-            <GrPrevious onClick={handleLeft} />
+        <div className="opacity-0  hover:opacity-50  z-50">
+          <span className=" absolute  top-[10rem] z-50  text-[30px] text-white  pl-4">
+            <GrPrevious
+              onClick={() =>
+                handleLeft(setCurrentState, data.plans.random?.length - 3)
+              }
+            />
           </span>
-          <span className="fixed  top-[18rem]  z-50 text-[20px] right-3 ">
-            <GrNext onClick={HandleRightClik} />
+          <span className="absolute  top-[10rem]  z-50 text-[30px] text-white  right-3 ">
+            <GrNext
+              onClick={() =>
+                HandleRight(setCurrentState, data.plans.random?.length - 3)
+              }
+            />
           </span>
         </div>
-        <div className={`flex flex-row   gap-2 w-full  `}>
+        <div className={`flex flex-row   gap-3 w-full   `}>
           {data.plans.random
-            ?.slice(currentState, currentState + data.plans.random?.length)
+            ?.slice(currentState, currentState + 3)
             .map((plan: planCardProps, index: number) => (
-              <PlanCardTwo item={plan} key={index} isClick={isClicked} />
+              <PlanCardTwo item={plan} key={index} prop={styleCarousel} />
+            ))}
+        </div>
+      </section>
+
+      <section className="w-full my-5 relative">
+        <div className=" opacity-0  hover:opacity-50">
+          <span className=" absolute  top-[4rem] z-50  text-[20px] pl-4">
+            <GrPrevious
+              onClick={() => handleLeft(setCurrenntSlid, categories.length - 5)}
+            />
+          </span>
+          <span className="absolute  top-[4rem]  z-50 text-[20px] right-3 ">
+            <GrNext
+              onClick={() =>
+                HandleRight(setCurrenntSlid, categories.length - 5)
+              }
+            />
+          </span>
+        </div>
+        <div className="w-full flex gap-4">
+          {categories
+            .slice(currenntSlid, currenntSlid + 5)
+            .map((item, index) => (
+              <div
+                key={index}
+                className=" p-4 md:w-[370px] h-[125px] md:h-[150px] w-[245px]  rounded-md flex justify-center items-center gap-3"
+                style={{ backgroundImage: `${item.backgroundImage}` }}
+              >
+                <p className=" border-2 p-5 w-24 h-8 flex items-center justify-center text-white">
+                  {" "}
+                  {item.name}
+                </p>
+              </div>
             ))}
         </div>
       </section>
